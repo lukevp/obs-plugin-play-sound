6f6e5ca05567092818d96da16c17010f0455be7e
ubiquitous-dev <luke@candldev.com>
2023-01-17 17:35:33 -0800

    Monitor connection to OBS and reconnect automatically

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ import OBSWebSocket, {EventSubscription} from 'obs-websocket-js';
 const obs = new OBSWebSocket();
 import { exec } from 'child_process';
 
+var isConnected = false;
+
+var setConnected = () => {
+    if (!isConnected) {
+        console.log("Connected to OBS!");
+    }
+    isConnected = true;
+}
+var setNotConnected = () => {
+    if (isConnected) {
+        console.log("Disconnected from OBS!");
+    }
+    isConnected = false;
+}
+
 function onRecordStateChanged(event) {
     console.log('Recording State Event Received: ', event.outputState);
     if (event.outputState === "OBS_WEBSOCKET_OUTPUT_STARTING" || event.outputState === "OBS_WEBSOCKET_OUTPUT_RESUMED") {
@@ -14,13 +29,25 @@ function onRecordStateChanged(event) {
 
 obs.on('RecordStateChanged', onRecordStateChanged);
 
-obs.once('ExitStarted', () => {
-  console.log('OBS started shutdown');
+obs.on("ConnectionOpened", setConnected);
+obs.on("ConnectionClosed", setNotConnected);
+obs.on("ConnectionError", setNotConnected);
+obs.on('ExitStarted', setNotConnected);
 
-  // Just for example, not necessary should you want to reuse this instance by re-connect()
-  obs.off('RecordStateChanged', onRecordStateChanged);
-});
-
+const sleep = ms => new Promise(res => setTimeout(res, ms))
 
 // connect to obs-websocket running on localhost with same port
-await obs.connect("ws://127.0.0.1:4455", process.argv[2]);
+while (true)
+{
+    if (!isConnected) {  
+        try {
+            await obs.connect("ws://127.0.0.1:4455", process.argv[2]);
+            setConnected();
+        }
+        catch (e) {
+            // console.log(e);
+            setNotConnected();
+        }
+    }
+    await sleep(1000);
+}
